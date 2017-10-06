7bb0c64328811cd3a55831875f985928895e0f93
AndrewLiuRM <andrewliurm@gmail.com>
2017-10-06 00:43:27 -0700

    v2.0
    Extra and Extra extra credit done.

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -5,26 +5,13 @@
 //  Created by Andrew Liu on 10/5/17.
 //  Copyright © 2017 Andrew Liu. All rights reserved.
 //
+// This project is added with Extra and Extra extra credits.
 
 import Foundation
 
-print("Enter an expression separated by returns:")
 
-var input = readLine(strippingNewline: true)!
-var data = input.split(separator: " ")
-var length = data.count
-if length == 0 {
-    print("Please input something.")
-} else if length == 1 {
+func calc(sign: String, front: Double, end: Double) {
     var result = 0.0
-    let front = Double(input)!
-    //let front = UInt.init(input)!
-    
-    let sign = readLine(strippingNewline: true)!
-    
-    let end = Double(readLine(strippingNewline: true)!)!
-    //let end = UInt.init(readLine(strippingNewline: true)!)!
-    
     switch sign {
     case "+" :
         result = front + end
@@ -50,32 +37,81 @@ if length == 0 {
         print("Please input a valid operator")
         break
     }
-} else {
-    let operate = data[length - 1]
-    switch operate {
-    case "count":
-        print(length - 1)
-        break
-    case "avg":
-        var sum = 0
-        for n in 0...(length - 2) {
-            sum += Int(UInt.init(data[n])!)
-        }
-        print(sum / (length - 1))
-        break
-    case "fact":
-        var num = UInt.init(data[0])!
-        var sum = 1
-        for n in 1...num {
-            sum = sum * Int(n)
-        }
-        print(sum)
-        break
-    default:
-        print("Invalid arguement")
-    }
-
 }
 
 
+print("Enter an expression separated by returns:")
+
+var input = readLine(strippingNewline: true)!
+var data = input.split(separator: " ")
+var length = data.count
+if length == 0 {
+    print("Please input something.")
+} else if length == 1 {
+    //var result = 0.0
+    let front = Double(input)!
+    //let front = UInt.init(input)!
+    
+    let sign = readLine(strippingNewline: true)!
+    
+    let end = Double(readLine(strippingNewline: true)!)!
+    //let end = UInt.init(readLine(strippingNewline: true)!)!
+    
+    calc(sign: sign, front: front, end: end)
+    
+} else {
+    let operate = data[length - 1]
+    if data[0] == "calc" {
+        switch operate {
+        case "count":
+            print(length - 2)
+            break
+        case "avg":
+            var sum = 0
+            for n in 1...(length - 2) {
+                sum += Int(UInt.init(data[n])!)
+            }
+            print(sum / (length - 2))
+            break
+        case "fact":
+            var num = UInt.init(data[1])!
+            var sum = 1
+            for n in 1...num {
+                sum = sum * Int(n)
+            }
+            print(sum)
+            break
+        default:
+            let front = Double(data[1])!
+            let sign = String(data[2])
+            let end = Double(data[3])!
+            
+            calc(sign: sign, front: front, end: end)
+        }
+    } else {
+        switch operate {
+        case "count":
+            print(length - 1)
+            break
+        case "avg":
+            var sum = 0
+            for n in 0...(length - 2) {
+                sum += Int(UInt.init(data[n])!)
+            }
+            print(sum / (length - 1))
+            break
+        case "fact":
+            var num = UInt.init(data[0])!
+            var sum = 1
+            for n in 1...num {
+                sum = sum * Int(n)
+            }
+            print(sum)
+            break
+        default:
+            print("Invalid arguement")
+        }
+    }
+
+}
 
